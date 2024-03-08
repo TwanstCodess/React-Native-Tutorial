7c608568426c824317b997d7202a4ab89bd4bc7c
Twanst Codes <155393812+TwanstCodess@users.noreply.github.com>
2024-03-08 23:34:12 +0300

    Text

--- Tutorial/App.js
+++ Tutorial/App.js
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import {  View } from 'react-native';
+import {  View ,Text} from 'react-native';
 
 export default function App() {
   return (
-    <View style={{flex:1,backgroundColor:"plum"}}>
+    <View style={{flex:1,backgroundColor:"plum",alignItems:"center",justifyContent:"center"}}>
 
-    <View style={{width:200,height:200,backgroundColor:"red"}}></View>
-    <View style={{width:200,height:200,backgroundColor:"green"}}></View>
-    <View style={{width:200,height:200,backgroundColor:"yellow"}}></View>
-    <View style={{width:200,height:200,backgroundColor:"teal"}}></View>
+ <Text style={{fontSize:30,paddingTop:30,marginTop:100,backgroundColor:"white"}}>Hello World IM <Text style={{color:"teal"}}>Twanst Codes</Text> </Text>
+
     </View>
   );
 }
