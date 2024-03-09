d119c3f3e77e0c06349a54386a04bf9615c5f4f2
Twanst Codes <155393812+TwanstCodess@users.noreply.github.com>
2024-03-09 15:28:04 +0300

    Pressable

--- Tutorial/App.js
+++ Tutorial/App.js
@@ -1,16 +1,36 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import {  View ,Text,Image, ImageBackground,ScrollView,Button} from 'react-native';
+import {  View ,Text,Image, ImageBackground,ScrollView,Button , Pressable, Modal} from 'react-native';
 const Imagest=require("./assets/favicon.png");
 
 export default function App() {
+  const [ISmodalVisible, setIsModalVisible] =useState(false);
   return (
 <View style={{flex:1,backgroundColor:"plum",padding:30,paddingTop:100}}>
 <Button title="Click me"
- onPress={()=>alert('Button Click')} 
+ onPress={()=>setIsModalVisible()} 
  color={"midnightblue"}
- disabled={false}
  
 />
+
+
+
+<Modal 
+visible={ISmodalVisible}
+onRequestClose={()=>setIsModalVisible(false)}
+animationType='slide'
+presentationStyle='pageSheet'
+>
+<View style={{flex:1,backgroundColor:"midnightblue",padding:30,paddingTop:100}}>
+<Text style={{color:"white"}}>Hello World Im Twanst Developer</Text>
+<Button title="X"
+ onPress={()=>setIsModalVisible(false)} 
+ color={"red"}
+ 
+/>
+</View>
+
+</Modal>
 </View>
   );
 }
