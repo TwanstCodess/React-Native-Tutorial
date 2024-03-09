ae37beafa0879ca7b380e39f9745bc110e09cf62
Twanst Codes <155393812+TwanstCodess@users.noreply.github.com>
2024-03-09 15:39:44 +0300

    ActivityIndicator And Loding

--- Tutorial/App.js
+++ Tutorial/App.js
@@ -1,12 +1,16 @@
 import { useState } from 'react';
-import {  View ,Text,Image, ImageBackground,ScrollView,Button , Pressable, Modal,StatusBar} from 'react-native';
+import {  View ,Text,Image, ImageBackground,ScrollView,Button , Pressable, Modal,StatusBar,ActivityIndicator} from 'react-native';
 const Imagest=require("./assets/favicon.png");
 
 export default function App() {
   const [ISmodalVisible, setIsModalVisible] =useState(false);
   return (
 <View style={{flex:1,backgroundColor:"plum",padding:60}}>
-<StatusBar backgroundColor="lightblue" />
+<ActivityIndicator 
+size="large"
+color="blue"
+animating={true}
+/>
 </View>
   );
 }
