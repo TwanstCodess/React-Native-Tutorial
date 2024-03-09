c4e5387471ff8f7ce57de9a2e537cfd63a7ae70f
Twanst Codes <155393812+TwanstCodess@users.noreply.github.com>
2024-03-09 15:46:35 +0300

    Alert

--- Tutorial/App.js
+++ Tutorial/App.js
@@ -1,16 +1,22 @@
 import { useState } from 'react';
-import {  View ,Text,Image, ImageBackground,ScrollView,Button , Pressable, Modal,StatusBar,ActivityIndicator} from 'react-native';
+import {  View ,Text,Image, ImageBackground,ScrollView,Button , Pressable, Modal,StatusBar,ActivityIndicator,Alert} from 'react-native';
 const Imagest=require("./assets/favicon.png");
 
 export default function App() {
   const [ISmodalVisible, setIsModalVisible] =useState(false);
   return (
 <View style={{flex:1,backgroundColor:"plum",padding:60}}>
-<ActivityIndicator 
-size="large"
-color="blue"
-animating={true}
-/>
+<Button title='Click Me ' onPress={() => Alert.alert('Button Clicked','Hello',[
+  {
+    text: 'Cancel',
+    onPress: () => console.log('Cancel Pressed'),
+    style: 'cancel',
+  },
+  {
+    text: 'OK',
+    onPress: () => console.log('OK Pressed'),
+  },
+])} />
 </View>
   );
 }
