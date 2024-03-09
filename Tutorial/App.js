687c2008979be6b54cc32b9557daea9c87545076
Twanst Codes <155393812+TwanstCodess@users.noreply.github.com>
2024-03-09 14:39:55 +0300

    Button

--- Tutorial/App.js
+++ Tutorial/App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import {  View ,Text,Image, ImageBackground,ScrollView} from 'react-native';
+import {  View ,Text,Image, ImageBackground,ScrollView,Button} from 'react-native';
 const Imagest=require("./assets/favicon.png");
 
 export default function App() {
   return (
-<View style={{flex:1,backgroundColor:"plum",padding:30}}>
-<ScrollView >
-
-{/* static Image */}
-
-
-<Image source={{ uri: 'https://picsum.photos/300' }} style={{ width: 300, height: 300 }}>
-  </Image>
-<Text>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups. </Text>
-
-    </ScrollView>
+<View style={{flex:1,backgroundColor:"plum",padding:30,paddingTop:100}}>
+<Button title="Click me"
+ onPress={()=>alert('Button Click')} 
+ color={"midnightblue"}
+ disabled={false}
+ 
+/>
 </View>
   );
 }
