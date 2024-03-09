4700ab9e11318da4ac638e5d84d2640e13c42551
Twanst Codes <155393812+TwanstCodess@users.noreply.github.com>
2024-03-09 16:05:44 +0300

    Custom Components and Layout

--- Tutorial/App.js
+++ Tutorial/App.js
@@ -1,22 +1,14 @@
 import { useState } from 'react';
 import {  View ,Text,Image, ImageBackground,ScrollView,Button , Pressable, Modal,StatusBar,ActivityIndicator,Alert} from 'react-native';
+import Greet from './components/Greet';
 const Imagest=require("./assets/favicon.png");
 
 export default function App() {
   const [ISmodalVisible, setIsModalVisible] =useState(false);
   return (
 <View style={{flex:1,backgroundColor:"plum",padding:60}}>
-<Button title='Click Me ' onPress={() => Alert.alert('Button Clicked','Hello',[
-  {
-    text: 'Cancel',
-    onPress: () => console.log('Cancel Pressed'),
-    style: 'cancel',
-  },
-  {
-    text: 'OK',
-    onPress: () => console.log('OK Pressed'),
-  },
-])} />
+<Greet name="Kakon"/> 
+<Greet name="kosar"/> 
 </View>
   );
 }
