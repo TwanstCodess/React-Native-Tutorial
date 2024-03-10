413dab193abc0cc0ea958344a4c832a1056ce26a
Twanst Codes <155393812+TwanstCodess@users.noreply.github.com>
2024-03-10 19:38:22 +0300

    Flex Wrap

--- Tutorial/App.js
+++ Tutorial/App.js
@@ -6,30 +6,32 @@ export default function App() {
   return (
     <View style={styles.container}>
 
-      <Greet childers="HI -1" style={{ backgroundColor: 'indigo', alignSelf: 'flex-start' }} />
-      <Greet childers="HI -2" style={{ backgroundColor: 'black', alignSelf: 'center' }} />
-      <Greet childers="HI -3" style={{ backgroundColor: 'teal', alignSelf: 'flex-end' }} />
-      <Greet childers="HI -4" style={{ backgroundColor: 'tomato', alignSelf: 'auto' }} />
-      <Greet childers="Hello" style={{ backgroundColor: 'yellow' }} />
-      <Greet childers="Hello" style={{ backgroundColor: 'pink' }} />
-      <Greet childers="Hello" style={{ backgroundColor: 'purple' }} />
-      <Greet childers="Hello" style={{ backgroundColor: 'lightblue' }} />
-      <Greet childers="Hello" style={{ backgroundColor: 'lightgreen' }} />
-      <Greet childers="Hello" style={{ backgroundColor: 'lightcoral' }} />
-
-
+      <Greet childers="HI -1" style={{ backgroundColor: 'indigo' }} />
+      <Greet childers="HI -2" style={{ backgroundColor: 'black' }} />
+      <Greet childers="HI -3" style={{ backgroundColor: 'teal' }} />
+      <Greet childers="HI -4" style={{ backgroundColor: 'tomato' }} />
+      <Greet childers="HI-5" style={{ backgroundColor: 'yellow' }} />
+      <Greet childers="HI-6" style={{ backgroundColor: 'pink' }} />
+      <Greet childers="HI-7" style={{ backgroundColor: 'purple' }} />
+      <Greet childers="HI-8" style={{ backgroundColor: 'lightblue' }} />
+      <Greet childers="HI-9" style={{ backgroundColor: 'lightgreen' }} />
+      <Greet childers="HI-10" style={{ backgroundColor: 'blue' }} />
+      <Greet childers="HI-11" style={{ backgroundColor: 'green' }} />
+      <Greet childers="HI-12" style={{ backgroundColor: 'goldenrod' }} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
+    // flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap-reverse',
+    height: "100%",
     backgroundColor: 'plum',
     marginTop: 40,
     marginBottom: 40,
     borderWidth: 5,
-    borderColor: 'green',
+    borderColor: 'gray',
   }
 });
