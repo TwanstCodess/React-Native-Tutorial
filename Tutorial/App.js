28bf42b1cd0ba9507e5a6b842a92fa912c366590
Twanst Codes <155393812+TwanstCodess@users.noreply.github.com>
2024-03-10 13:01:28 +0300

    StyleSheet

--- Tutorial/App.js
+++ Tutorial/App.js
@@ -1,20 +1,15 @@
-import { useState } from 'react';
-import {  View ,Text,Image, ImageBackground,ScrollView,Button , Pressable, Modal,StatusBar,ActivityIndicator,Alert} from 'react-native';
-import Greet from './components/Greet';
-const Imagest=require("./assets/favicon.png");
-
+import {  View ,Text,Image, ImageBackground,ScrollView,Button , Pressable, Modal,StatusBar,ActivityIndicator,Alert,styleSheet, StyleSheet} from 'react-native';
 export default function App() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const toggleModal = () => {
-    setIsModalVisible(!isModalVisible);
-  };
-  return (
-<View style={{flex:1,backgroundColor:"plum",padding:60}}>
-<Greet name="Twanst Codes" age="12"/> 
-<Button title='Twanst' onPress={toggleModal} />
-<ActivityIndicator size={"large"} color="red" animating={isModalVisible} />
 
+  return (
+<View style={styles.container}>
+<Text style={styles.title}>Api Style</Text>
 </View>
   );
 }
 
+const styles=StyleSheet.create({
+
+container:{ flex:1, backgroundColor:"plum",  padding:60},
+title:{ fontSize: 24, fontWeight: 'bold', textAlign: 'center' , color:"lightblue",marginTop:40},
+});
