16b3bbc21916d2568da18aca89b1703c85e86472
Twanst Codes <155393812+TwanstCodess@users.noreply.github.com>
2024-03-10 21:39:39 +0300

    List

--- Tutorial/App.js
+++ Tutorial/App.js
@@ -1,30 +1,62 @@
 import React from 'react';
-import { View, StyleSheet, Text, SafeAreaView } from 'react-native';
+import { View, StyleSheet, Text, SafeAreaView, ScrollView } from 'react-native';
+import data from './data/list.json';
 
 export default function App() {
-
   return (
-    <SafeAreaView style={styles.SafyAreaContainer}>
-      <View style={styles.container}>
-        <View style={styles.box}>
-          <Text style={styles.text}>Welcome To Tutorial</Text>
-        </View>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView horizontal={true} contentContainerStyle={styles.scrollContainer}>
+        {data.map((item) => (
+          <View style={styles.card} key={item.id}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.description}>{item.description}</Text>
+          </View>
+        ))}
+      </ScrollView>
+      <ScrollView contentContainerStyle={styles.scrollContainers}>
+        {data.map((item) => (
+          <View style={styles.cards} key={item.id}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.description}>{item.description}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
-};
-
+}
 
 const styles = StyleSheet.create({
-  SafyAreaContainer: {
-    flex: 1,
-    backgroundColor: 'lightblue',
-  },
   container: {
     flex: 1,
-    backgroundColor: 'lightblue',
+    backgroundColor: 'teal',
+  },
+  scrollContainer: {
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  scrollContainers: {
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  card: {
+    backgroundColor: 'white',
+    padding: 20,
+    marginHorizontal: 10,
+    borderRadius: 15,
+    shadowColor: '#000',
+
+  },
+  cards: {
+    backgroundColor: 'white',
+    padding: 20,
+    marginVertical: 10,
+    borderRadius: 15,
+    shadowColor: '#000',
+
   },
 
 });
