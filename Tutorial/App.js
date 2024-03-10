11478403b03f0bec270b5b6a197526396abe5929
Twanst Codes <155393812+TwanstCodess@users.noreply.github.com>
2024-03-10 14:33:56 +0300

    flexDirection column and row reversie

--- Tutorial/App.js
+++ Tutorial/App.js
@@ -6,15 +6,16 @@ export default function App() {
   return (
     <View style={styles.container}>
 
-      <Greet childers="Hello" style={{ backgroundColor: 'indigo' }} />
-      <Greet childers="Hello" style={{ backgroundColor: 'black' }} />
-      <Greet childers="Hello" style={{ backgroundColor: 'teal' }} />
-      {/* <Greet childers="Hello" style={{ backgroundColor: 'yellow' }} />
+      <Greet childers="HI -1" style={{ backgroundColor: 'indigo' }} />
+      <Greet childers="HI -2" style={{ backgroundColor: 'black' }} />
+      <Greet childers="HI -3" style={{ backgroundColor: 'teal' }} />
+      <Greet childers="HI -4" style={{ backgroundColor: 'tomato' }} />
+      <Greet childers="Hello" style={{ backgroundColor: 'yellow' }} />
       <Greet childers="Hello" style={{ backgroundColor: 'pink' }} />
       <Greet childers="Hello" style={{ backgroundColor: 'purple' }} />
       <Greet childers="Hello" style={{ backgroundColor: 'lightblue' }} />
       <Greet childers="Hello" style={{ backgroundColor: 'lightgreen' }} />
-      <Greet childers="Hello" style={{ backgroundColor: 'lightcoral' }} /> */}
+      <Greet childers="Hello" style={{ backgroundColor: 'lightcoral' }} />
 
 
     </View>
@@ -24,11 +25,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column-reverse',
+    flexDirection: 'column',
     backgroundColor: 'plum',
     marginTop: 40,
     marginBottom: 40,
     borderWidth: 5,
-    borderColor: 'red',
+    borderColor: 'green',
   }
 });
