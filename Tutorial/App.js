fcec5a55c0483797799a0d9b4da0d1e05b36eee9
Twanst Codes <155393812+TwanstCodess@users.noreply.github.com>
2024-03-10 13:21:38 +0300

    Box Model

--- Tutorial/App.js
+++ Tutorial/App.js
@@ -1,38 +1,51 @@
-import {  View ,Text,Image, ImageBackground,ScrollView,Button , Pressable, Modal,StatusBar,ActivityIndicator,Alert,styleSheet, StyleSheet} from 'react-native';
+import { View, Text, Image, ImageBackground, ScrollView, Button, Pressable, Modal, StatusBar, ActivityIndicator, Alert, styleSheet, StyleSheet } from 'react-native';
 export default function App() {
 
   return (
-<View style={styles.container}>
+    <View style={styles.container}>
 
 
-<View style={[styles.lightBlue,styles.Box]}>
-<Text>Hello World Im Twanst </Text>
-</View>
+      <View style={[styles.lightBlue, styles.Box]}>
+        <Text style={styles.Title}>Hello World Im Twanst </Text>
+      </View>
 
-<View style={[styles.lightgreen,styles.Box]}>
-<Text>Hello World Im Twanst </Text>
-</View>
+      <View style={[styles.lightgreen, styles.Box]}>
+        <Text style={styles.Title}>Hello World Im Twanst </Text>
+      </View>
 
 
-</View>
+    </View>
   );
 }
 
-const styles=StyleSheet.create({
+const styles = StyleSheet.create({
 
-container:{ flex:1, backgroundColor:"plum",  padding:60},
-Box:{
-  height:100,
-  width:100,
-  padding:10, 
-  marginTop:3,
-  borderRadius:10,
-},
-lightBlue:{
- backgroundColor:"lightblue",
+  container: { flex: 1, backgroundColor: "plum", padding: 60 },
+  Box: {
+    height: 100,
+    width: 100,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    marginVertical: 10,
+    borderWidth: 2,
+    borderColor: "black",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
 
-},
-lightgreen:{
- backgroundColor:"lightgreen",
-}
+  },
+  lightBlue: {
+    backgroundColor: "lightblue",
+
+  },
+  lightgreen: {
+    backgroundColor: "lightgreen",
+  },
+  Title: {
+    fontFamily: "sans-serif",
+    fontSize: 12,
+    color: "white",
+    backgroundColor: "black",
+
+  },
 });
